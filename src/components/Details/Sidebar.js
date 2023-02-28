b0020f10f492d5f1031abfb0a7f452c2d40b1b1b
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-02-28 23:03:15 +0100

    Add sidebar

---
+++ src/components/Details/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Details.css';
+
+const Sidebar = () => (
+  <div className="sidebar">
+    <div className="sidebar-header">Cars#show</div>
+    <div className="sidebar-content">
+      <li>MODEL</li>
+      <li>LIFESTYLE</li>
+      <li>SHOP</li>
+      <li>TESTDRIVE</li>
+    </div>
+  </div>
+);
+
+export default Sidebar;
