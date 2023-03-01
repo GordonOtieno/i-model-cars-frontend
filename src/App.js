a29d2047226f1fe707074c0b431aa45c032affad
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-03-01 16:12:16 +0100

    Modify to include SignUp component only

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Main from './components/Main/Main';
-import Details from './components/Details/Details';
 import SignUpForm from './components/SignUp/SignUp';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<SignUpForm />} />
-    <Route path="/Main" element={<Main />} />
-    <Route path="http://127.0.0.1:3000/api/v1/cars/:id" element={<Details />} />
   </Routes>
 );
 
