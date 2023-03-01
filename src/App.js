6c8d431a16c1de605ad0aaf66ead64b8a766dc94
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-03-01 08:06:38 +0100

    Update route to include SignUp component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Main from './components/Main/Main';
 import Details from './components/Details/Details';
+import SignUpForm from './components/SignUp/SignUp';
 
 const App = () => (
   <Routes>
-    <Route path="/" element={<Main />} />
+    <Route path="/" element={<SignUpForm />} />
+    <Route path="" element={<Main />} />
     <Route path="http://127.0.0.1:3000/api/v1/cars/:id" element={<Details />} />
   </Routes>
 );
