c81ed12514fde3e4b3dfddf6ec9aa23d1ef8bb42
Amine <ouas.amine@outlook.com>
2023-03-01 19:19:50 +0100

    Style new reservation form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NewReservationForm from './components/reservations/NewReservationForm';
 import SignUpForm from './components/SignUp/SignUp';
+import './App.css';
 
 const App = () => (
   <Routes>
