adacbbfed2306cf6e93047d4af2ea0cde72c2437
Amine <ouas.amine@outlook.com>
2023-03-01 20:31:05 +0100

    Implement interactive messages

--- src/components/reservations/NewReservationForm.js
+++ src/components/reservations/NewReservationForm.js
@@ -9,15 +9,27 @@ export default function NewReservationForm(props) {
   // Set initial fields values
   const [reservation, setReservation] = useState({
     car_id: 0,
-    user_id: 0,
     date: '',
     city: '',
+  });
+
+  // Set message
+  const [msg, setMsg] = useState({
+    type: '',
+    text: '',
   });
 
   // Send the reservation details to the API
   const handleSubmit = (e) => {
     e.preventDefault();
-    createReservationAPI(1, reservation);
+    createReservationAPI(1, reservation)
+      .then((response) => {
+        if (response.status === 200) {
+          setMsg({ type: 'msg-sucss', text: 'The car has been booked' });
+        } else {
+          setMsg({ type: 'msg-fail', text: 'Something went wrong!' });
+        }
+      });
   };
 
   // Update the state after each change in the fields values
@@ -30,11 +42,11 @@ export default function NewReservationForm(props) {
       <h1>BOOK ONE OF OUR CARS FOR A TEST DRIVE</h1>
       <form>
         <input type="number" name="car_id" placeholder="Car Id" value={reservation.car_id} onChange={handleFieldChange} />
-        <input type="number" name="user_id" placeholder="User id" value={reservation.user_id} onChange={handleFieldChange} />
         <input type="date" name="date" placeholder="date" value={reservation.date} onChange={handleFieldChange} />
         <input type="text" name="city" placeholder="city" value={reservation.city} onChange={handleFieldChange} />
         <input className="buttn" type="submit" value="Book Now" onClick={handleSubmit} />
       </form>
+      <span className={msg.type}>{ msg.text }</span>
     </div>
   );
 }
