2d8c186c9430fcbd5c7371353145e226b7e2325d
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-03-02 18:16:44 +0100

    Update component to include Main page only

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Main from './components/Main/Main';
-import Details from './components/Details/Details';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Main />} />
-    <Route path="/Details/:id" element={<Details />} />
   </Routes>
 );
 
