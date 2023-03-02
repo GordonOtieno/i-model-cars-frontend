ede534a29c6d656c470f5bad8f9f6e04b78a3e81
Amine <ouas.amine@outlook.com>
2023-03-02 13:29:35 +0100

    Create Resevations component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Routes, Route } from 'react-router-dom';
 import NewReservationForm from './components/reservations/NewReservationForm';
 import SignUpForm from './components/SignUp/SignUp';
 import './App.css';
+import Reservations from './components/reservations/Reservations';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<SignUpForm />} />
+    <Route path="/reservations" element={<Reservations />} />
     <Route path="/reservations/new" element={<NewReservationForm />} />
   </Routes>
 );
---
+++ src/components/reservations/Reservations.js
@@ -0,0 +1,7 @@
+export default function Reservations() {
+  return (
+    <div>
+      <h1>All Reservations</h1>
+    </div>
+  );
+}
