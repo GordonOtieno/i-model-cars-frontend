6cb180ef029af8fbd5b18ebfe7c5aca95a376148
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-03-02 11:57:32 +0100

    Modify Main component layout

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,36 +1,31 @@
-import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
-import Sidebar from '../Details/Sidebar';
+import React, { useEffect, useState } from 'react';
 
 const Main = () => {
   const [cars, setCars] = useState([]);
-
+  // let's fetch data from Api
   useEffect(() => {
-    // get cars from the api
-    // update cars state
     axios.get('http://127.0.0.1:3000/api/v1/cars')
       .then((response) => {
-        console.log(response.data);
         setCars(response.data);
       })
       .catch((response) => console.log(response));
-  }, [cars.length]);
+  }, []);
 
   return (
-    <main>
-      <Sidebar />
-      {cars.map((car) => (
-        <div key={car.id} className="content">
-          <img src={car.image} alt={car.name} />
-          <p>{car.name}</p>
-          <hr />
-          {' '}
-          <p className="car-make">{car.make}</p>
-          <Link to={`/Details/${car.id}`}>View</Link>
-        </div>
-      ))}
-    </main>
+    <>
+      <section>
+        <header>
+          <h1>Latest i-Model Cars</h1>
+        </header>
+        {cars.map((car) => (
+          <div key={car.id} className="cars">
+            <img src={car.images.dark} alt={car.name} />
+            <span>{car.name}</span>
+          </div>
+        ))}
+      </section>
+    </>
   );
 };
 
