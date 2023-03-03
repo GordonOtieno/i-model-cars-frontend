0b84a7f809137adf109150c6d5a9dc64e291168e
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-03-03 10:45:00 +0100

    Update Route in the app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NewReservationForm from './components/reservations/NewReservationForm';
 import SignUpForm from './components/SignUp/SignUp';
+import Details from './components/details/Details';
 import './App.css';
 
 const App = () => (
   <Routes>
-    <Route path="/" element={<SignUpForm />} />
+    <Route path="/" element={<Details />} />
+    <Route path="/main" element={<SignUpForm />} />
     <Route path="/reservations/new" element={<NewReservationForm />} />
   </Routes>
 );
