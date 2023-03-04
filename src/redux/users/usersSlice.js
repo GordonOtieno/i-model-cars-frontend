df4410e902f1b83fb95157ea6185acd153ed503a
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-03-04 19:56:44 +0100

    Update userSlice

--- src/redux/users/usersSlice.js
+++ src/redux/users/usersSlice.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { FaAcquisitionsIncorporated } from 'react-icons/fa';
 
 const SIGNUP_URL = 'http://localhost:3000/signup';
 const LOGIN_URL = 'http://localhost:3000/login';
@@ -7,44 +8,38 @@ const USER_SIGNED_UP = 'user_signup';
 const USER_LOGGED_IN = 'user_login';
 const USER_LOGGED_OUT = 'user_logged_out';
 
-
 const user = async (url, userData) => {
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify(userData),
-    });
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
 
-    if (!response.ok) {
-      throw new Error('Failed to authenticate');
-    }
-
-    const token = response.headers.get('authorized');
-    localStorage.setItem('token', JSON.stringify(token));
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    throw error;
+  if (!response.ok) {
+    throw new Error('Failed to authenticate');
   }
-};
 
+  const token = response.headers.get('authorized');
+  localStorage.setItem('token', JSON.stringify(token));
+
+  const data = await response.json();
+  return data;
+};
 
 // Sign up user
 export const userSignUp = createAsyncThunk(USER_SIGNED_UP, async (userData) => {
-  const data = await user (SIGNUP_URL, userData);
+  const data = await user(SIGNUP_URL, userData);
   return data;
 });
 
 // Sign in user
 export const userLogin = createAsyncThunk(USER_LOGGED_IN, async (userData) => {
-  const data = await user (LOGIN_URL, userData);
+  const data = await user(LOGIN_URL, userData);
   return data;
-})
+});
 
 // Log out user
 export const userLogout = createAsyncThunk(USER_LOGGED_OUT, async () => {
@@ -52,15 +47,15 @@ export const userLogout = createAsyncThunk(USER_LOGGED_OUT, async () => {
   if (!token) {
     throw new Error('No token found in localStorage');
   }
-  
+
   const response = fetch(LOGOUT_URL, {
     method: 'delete',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': token,
+      Authorization: token,
     },
   });
-  
+
   const data = (await response).json();
   localStorage.removeItem('token');
   return data;
@@ -68,25 +63,18 @@ export const userLogout = createAsyncThunk(USER_LOGGED_OUT, async () => {
 
 const userSlice = createSlice({
   name: 'users',
-  initialState: {message: '', data: {}, isLogged: false },
+  initialState: { message: '', data: {}, isLogged: false },
   reducers: {},
   extraReducers: (builder) => {
-    builder
-      .addCase(userSignUp.fulfilled, (state, action) => {
-        state.message = action.payload.status.message;
-        state.data = action.payload.data;
-        state.isLogged = true;
-      })
-      .addCase(userLogin.fulfilled, (state, action) => {
-        state.message = action.payload.status.message;
-        state.data = action.payload.data;
-        state.isLogged = true;
-      })
-      .addCase(userLogout.fulfilled, (state, action) => {
-        state.message = action.payload.message;
-        state.data = {};
-        state.isLogged = false;
-      });
+    builder.addCase(userSignUp.fulfilled, (state, action) => ({
+      ...state, message: action.payload.status.message, data: action.payload.data, isLogged: true,
+    }));
+    builder.addCase(userLogin.fulfilled, (state, action) => ({
+      ...state, message: action.payload.status.message, data: action.payload.data, isLogged: true,
+    }));
+    builder.addCase(userLogout.fulfilled, (state, action) => ({
+      ...state, message: action.payload.message, data: {}, isLogged: false,
+    }));
   },
 });
 
