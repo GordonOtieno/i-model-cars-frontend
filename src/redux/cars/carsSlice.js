19d27a0b875f3c8ef95c3373c6e7e670afaa1d75
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-03-04 19:57:17 +0100

    Modify carsSlice to use axios

--- src/redux/cars/carsSlice.js
+++ src/redux/cars/carsSlice.js
@@ -1,22 +1,22 @@
-/* eslint-disable no-param-reassign */
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { getCarsAPI } from '../helpers/cars-api';
+import axios from 'axios';
 
-export const getCarsThunk = createAsyncThunk('cars/fetchAll',
-  async () => {
-    const response = await getCarsAPI()
-      .then((res) => res.json())
-      .then((res) => res);
-    return response;
-  });
+export const getCarsThunk = createAsyncThunk('cars/fetchAll', async () => {
+  const response = await axios.get('http://127.0.0.1:3000/api/v1/cars');
+  return response.data;
+});
+
 const carsSlice = createSlice({
   name: 'cars',
-  initialState: { cars: {} },
+  initialState: { cars: [] },
+  reducers: {},
   extraReducers: (builder) => {
     builder.addCase(getCarsThunk.fulfilled, (state, action) => {
       state.cars = action.payload;
     });
   },
 });
+
+export const selectCars = (state) => state.cars.cars;
 
 export default carsSlice.reducer;
